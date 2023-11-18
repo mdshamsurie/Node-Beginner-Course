d700c676e369e0ae4e3c408d9385a5ef72248210
hatsyu <mdshamsurie@gmail.com>
2023-11-19 02:34:12 +0800

    add pakage nodemon

--- src/app.js
+++ src/app.js
@@ -12,6 +12,26 @@
 //     console.log('Server Running ....');
 // });
 
-const { v4: uuidv4 } = require('uuid');
+// const { v4: uuidv4 } = require('uuid');
 
-console.log(uuidv4());
+// console.log(uuidv4());
+
+//--------------------------------------//
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+//get request object
+app.get('/', (req, res) => {
+ res.send('Welcome to mypage!');
+});
+
+//post request object
+app.post('/', (req, res) => {
+ res.send('This is post request!');
+});
+
+app.listen(PORT, () => {
+
+    console.log('App listening on port '+ PORT);
+});
